65af982113388c756744b7b32e5e28a373a0cb33
vazha <gudenaffalkon@gmail.com>
2025-07-05 08:39:51 +0200

    Add Button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import {IconMail} from "@tabler/icons-react";
-import {Button} from "@/components/ui/button";
+'use client'
 
 export default function Home() {
     return (
-        <div className={'flex justify-center items-center h-dvh gap-5'}>
-            <IconMail strokeWidth={0.5}/>
-            <Button>Button</Button>
+        <div className={'flex flex-col flex-dored justify-center items-center h-dvh gap-5'}>
+            <div className="flex w-full flex-wrap gap-2">
+            </div>
+
         </div>
     );
 }
