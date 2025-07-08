2d6069ed0522e19e8fe98eaec54866da9805fd89
vazha <gudenaffalkon@gmail.com>
2025-07-08 18:25:33 +0200

    add Summary and Plans blocks

--- src/shared/store/rootReducer.ts
+++ src/shared/store/rootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import headerReducer from '@/widgets/Header/model/headerSlice'
+import summarySlice from "@/widgets/Summary/model/summarySlice";
 
 export const rootReducer = combineReducers({
     header: headerReducer,
+    summary: summarySlice,
 })
